21559d4d89cc5fe62b2db41f43d8709b40b69fb1
PATAN KHADIR <khadir@shipnext.in>
2019-12-20 22:26:12 +0530

    Updates

---
+++ precache-manifest.1e3496b16cccc6965f7a3a864b4fc8d9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a7243eafb41e61c7ed779f35c06f0a",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "28348b6b0cb52d94ae66",
+    "url": "/shopping-cart/static/css/main.c9bab6cb.chunk.css"
+  },
+  {
+    "revision": "35fa5a9a5ac60c9812e9",
+    "url": "/shopping-cart/static/js/2.dc23682a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/shopping-cart/static/js/2.dc23682a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28348b6b0cb52d94ae66",
+    "url": "/shopping-cart/static/js/main.19bd8b3c.chunk.js"
+  },
+  {
+    "revision": "13437d0b965bfbfef91f",
+    "url": "/shopping-cart/static/js/runtime-main.276815e1.js"
+  },
+  {
+    "revision": "f0f57bf2827a2387e2a535a9cebfa4df",
+    "url": "/shopping-cart/static/media/aashirvad.f0f57bf2.jpg"
+  },
+  {
+    "revision": "e0f7a96cf13c06386c7b3f5ba5fe93da",
+    "url": "/shopping-cart/static/media/akiva.e0f7a96c.jpg"
+  },
+  {
+    "revision": "2de1a6c01bbf9745d8f8c9873641c8f7",
+    "url": "/shopping-cart/static/media/amrutam.2de1a6c0.jpg"
+  },
+  {
+    "revision": "1766a06b0e753e3487429b1e82296bd7",
+    "url": "/shopping-cart/static/media/amul.1766a06b.jpg"
+  },
+  {
+    "revision": "7c36da2ac6d7f119643bf4f4e62f254b",
+    "url": "/shopping-cart/static/media/fresh.7c36da2a.jpg"
+  },
+  {
+    "revision": "9f7390ca2782eab7883ef291fe291bde",
+    "url": "/shopping-cart/static/media/mothersDairy.9f7390ca.jpg"
+  },
+  {
+    "revision": "0e6174dc51b94396093eb87ab67d6caf",
+    "url": "/shopping-cart/static/media/nandini.0e6174dc.jpg"
+  },
+  {
+    "revision": "88d0cd7c770d1411d70aa6d8adb52395",
+    "url": "/shopping-cart/static/media/nestle.88d0cd7c.jpg"
+  },
+  {
+    "revision": "734434f4ebab534ec1a39c05dea21c10",
+    "url": "/shopping-cart/static/media/patanjali.734434f4.jpg"
+  },
+  {
+    "revision": "3a2abf39fc8c77d75bb14c5a2d90e0b4",
+    "url": "/shopping-cart/static/media/srisri.3a2abf39.jpg"
+  }
+]);
